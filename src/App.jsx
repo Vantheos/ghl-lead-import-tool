42456d388af01fa79fba2caeb9d803f92fab40d5
Vantheos <ops@vantheos.com>
2026-04-13 19:04:20 -0400

    Fix download button not showing when few columns match mapping

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ export default function App() {
           </div>
         )}
 
-        {result?.csv && (
+        {result?.filename && (
           <DownloadButton csv={result.csv} filename={result.filename} />
         )}
 
